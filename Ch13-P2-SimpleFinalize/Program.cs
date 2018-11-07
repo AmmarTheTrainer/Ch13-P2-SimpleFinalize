8c63b7bfd33e4fb823371fdcb7ab42f7fd504662
Ammar The Trainer <ammarthetrainer@gmail.com>
2018-11-07 15:44:41 +0500

    Project Created

---
+++ Ch13-P2-SimpleFinalize/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ch13_P2_SimpleFinalize
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
