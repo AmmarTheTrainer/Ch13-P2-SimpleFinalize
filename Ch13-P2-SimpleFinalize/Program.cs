a77780572567a999af8dae4c39dda4e44702cf97
Ammar The Trainer <Ammarthetrainer@gmail.com>
2018-11-12 22:25:22 +0500

    final code update after class

--- Ch13-P2-SimpleFinalize/Program.cs
+++ Ch13-P2-SimpleFinalize/Program.cs
@@ -8,22 +8,35 @@ namespace Ch13_P2_SimpleFinalize
 {
     class Program
     {
-        ~Program()
-        {
-            Console.WriteLine(" prOGRAM CLASS Finalizer ");
-        }
+        
+        //~Program()
+        //{
+        //    object
+        //    Console.WriteLine(" prOGRAM CLASS Finalizer ");
+        //}
         static void Main(string[] args)
         {
             MyResourceWrapper rw = new MyResourceWrapper();
 
-            Console.WriteLine("***** Fun with Finalizers *****\n");
-            Console.WriteLine("Hit the return key to shut down this app");
-            Console.WriteLine("and force the GC to invoke Finalize()");
-            Console.WriteLine("for finalizable objects created in this AppDomain.");
+            //Console.WriteLine("***** Fun with Finalizers *****\n");
+            //Console.WriteLine("Hit the return key to shut down this app");
+            //Console.WriteLine("and force the GC to invoke Finalize()");
+            //Console.WriteLine("for finalizable objects created in this AppDomain.");
 
-            //MyResourceWrapper rw2 = new MyResourceWrapper();
+            ////MyResourceWrapper rw2 = new MyResourceWrapper();
 
-            //Program program = new Program();
+            ////Program program = new Program();
+            ///
+
+                using (Student stu = new Student())
+                {
+                    // logic , data 
+                }
+
+            Student stu2 = new Student();
+
+            //stu.Dispose();
+
             Console.ReadLine();
 
         }
---
+++ Ch13-P2-SimpleFinalize/Student.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Ch13_P2_SimpleFinalize
+{
+    class Student 
+    {
+        public Student()
+        {
+            Console.WriteLine(" ctor invoked ");
+        }
+
+        ~Student()
+        {
+            CleanUpLogic(); // call 
+            Console.WriteLine(" finalize() method invoked ");
+            Thread.Sleep(5000);
+            Console.ReadLine();
+        }
+        private bool IsDisposed;
+        private void CleanUpLogic()
+        {
+            // clean up logic for unmanaged resources
+        }
+
+        //public void Dispose()
+        //{
+        //    if (!IsDisposed)
+        //    {
+        //        CleanUpLogic();
+        //    }
+        //    // clean up logic for managed resources
+
+        //    Console.WriteLine(" Dispose() method invoked ");
+        //    GC.SuppressFinalize(this);
+        //    IsDisposed = true;
+        //}
+    }
+}
